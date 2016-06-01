2f15b5ece0dd54369a93b07d39f5290dd8e609e3
peterlin424 <peterlin424@gmail.com>
2016-06-01 17:39:42 +0800

    load md5 mesh and anim

--- app/src/main/java/com/example/linweijie/myrajawaliopengl/mRajawaliRenderer.java
+++ app/src/main/java/com/example/linweijie/myrajawaliopengl/mRajawaliRenderer.java
@@ -3,19 +3,18 @@ package com.example.linweijie.myrajawaliopengl;
 import android.app.Activity;
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.util.Log;
 import android.view.MotionEvent;
 
 import org.rajawali3d.Object3D;
+import org.rajawali3d.animation.mesh.SkeletalAnimationObject3D;
+import org.rajawali3d.animation.mesh.SkeletalAnimationSequence;
 import org.rajawali3d.lights.DirectionalLight;
 import org.rajawali3d.lights.PointLight;
-import org.rajawali3d.loader.LoaderOBJ;
-import org.rajawali3d.materials.Material;
-import org.rajawali3d.materials.methods.DiffuseMethod;
-import org.rajawali3d.materials.textures.Texture;
-
+import org.rajawali3d.loader.ParsingException;
+import org.rajawali3d.loader.md5.LoaderMD5Anim;
+import org.rajawali3d.loader.md5.LoaderMD5Mesh;
 import org.rajawali3d.materials.textures.VideoTexture;
-import org.rajawali3d.primitives.Cube;
-import org.rajawali3d.primitives.Plane;
 import org.rajawali3d.util.debugvisualizer.DebugVisualizer;
 import org.rajawali3d.util.debugvisualizer.GridFloor;
 
@@ -28,6 +27,8 @@ public class mRajawaliRenderer extends org.rajawali3d.renderer.RajawaliRenderer 
     public Context context;
 
     private Object3D watch;
+    private SkeletalAnimationObject3D mObject;
+    private SkeletalAnimationSequence mSequenceIdle;
     private ArcballCamera arcball;
 
     public static double INIT_MODEL_X = 0.0f;
@@ -39,7 +40,7 @@ public class mRajawaliRenderer extends org.rajawali3d.renderer.RajawaliRenderer 
     public static double INIT_CAMERA_Y = 50.0f;
     public static double INIT_CAMERA_Z = 50.0f;
 
-    private VideoTexture  mVideoTexture;
+    private VideoTexture mVideoTexture;
     private MediaPlayer mMediaPlayer;
 
     public mRajawaliRenderer(Context context) {
@@ -69,19 +70,17 @@ public class mRajawaliRenderer extends org.rajawali3d.renderer.RajawaliRenderer 
             debugViz.addChild(new GridFloor());
             getCurrentScene().addChild(debugViz);
 
-
             /**
              * 模型 mtl obj Object3D
              * */
-//            /** 載入模型 */
-////                final LoaderAWD parser = new LoaderAWD(mContext.getResources(), mTextureManager, R.raw.awd_suzanne);
-////                parser.parse();
+            /** 載入模型 */
+//                final LoaderAWD parser = new LoaderAWD(mContext.getResources(), mTextureManager, R.raw.awd_suzanne);
+//                parser.parse();
 //            final LoaderOBJ parser = new LoaderOBJ(mContext.getResources(), mTextureManager, R.raw.watch_obj);
 //            parser.parse();
-//
 //            watch = parser.getParsedObject();
-//
-//            /** 模型材質貼皮 */
+
+            /** 模型材質貼皮 */
 //            Material material = new Material();
 //            material.enableLighting(true);
 //            material.setDiffuseMethod(new DiffuseMethod.Lambert());
@@ -98,30 +97,71 @@ public class mRajawaliRenderer extends org.rajawali3d.renderer.RajawaliRenderer 
             /**
              * 影片 plane
              * */
-            // Create media player
-            mMediaPlayer = MediaPlayer.create(getContext(), R.raw.music_viedo);
+//            // Create media player
+//            mMediaPlayer = MediaPlayer.create(getContext(), R.raw.music_viedo);
+//
+//            // Create video texture and apply to material
+//            mVideoTexture = new VideoTexture("VideoTexture", mMediaPlayer);
+//            Material mVideo = new Material();
+//            mVideo.setColorInfluence(0);
+//            mVideo.addTexture(mVideoTexture);
+//
+//            // Create object
+//            watch = new Plane(3f,3f,5,5);
+//            watch.setMaterial(mVideo);
+//            watch.setPosition(INIT_MODEL_X, INIT_MODEL_Y + 2, INIT_MODEL_Z);
+//            wwatch0011_bitmap.jpgwatch.fbxatch.setScale(INIT_MODEL_SCALE);
+//
+//            watch.setRotZ(-90f); //  Rotate as video texture displays up-side-down
+//            watch.setRotY(180f); //  Rotate as video texture displays up-side-down
+//            getCurrentScene().addChild(watch);
+//            mMediaPlayer.setLooping(true);
 
-            // Create video texture and apply to material
-            mVideoTexture = new VideoTexture("VideoTexture", mMediaPlayer);
-            Material mVideo = new Material();
-            mVideo.setColorInfluence(0);
-            mVideo.addTexture(mVideoTexture);
+            /** FBX */
+//            LoaderFBX parser = new LoaderFBX(this, R.raw.bacteria2);
+//            parser.parse();
+//            watch = parser.getParsedObject();
+//            getCurrentScene().addChild(watch);
 
-            // Create object
-            watch = new Plane(3f,3f,5,5);
-            watch.setMaterial(mVideo);
-            watch.setPosition(INIT_MODEL_X, INIT_MODEL_Y + 2, INIT_MODEL_Z);
-            watch.setScale(INIT_MODEL_SCALE);
+            /** MD5 */
+            LoaderMD5Mesh meshParser = new LoaderMD5Mesh(this, R.raw.boblampclean_mesh);
+            meshParser.parse();
+            mObject = (SkeletalAnimationObject3D) meshParser.getParsedAnimationObject();
 
-            watch.setRotZ(-90f); //  Rotate as video texture displays up-side-down
-            watch.setRotY(180f); //  Rotate as video texture displays up-side-down
-            getCurrentScene().addChild(watch);
-            mMediaPlayer.setLooping(true);
+            LoaderMD5Anim animParser = new LoaderMD5Anim("boblampclean", this, R.raw.boblampclean_anim);
+            animParser.parse();
+            mSequenceIdle = (SkeletalAnimationSequence) animParser.getParsedAnimationSequence();
+
+            mObject.setAnimationSequence(mSequenceIdle);
+            mObject.setFps(24);
+            mObject.play();
+
+            getCurrentScene().addChild(mObject);
+
+            /** AWD */
+//            final LoaderAWD parser = new LoaderAWD(mContext.getResources(), mTextureManager, R.raw.boblampclean_anim_awd);
+//            parser.parse();
+//            mObject = (SkeletalAnimationObject3D) parser.getParsedObject();
+//            mObject.rotate(Vector3.Y, -90.0);
+//
+//            LoaderMD5Anim animParser = new LoaderMD5Anim("idle", this, R.raw.boblampclean_anim);
+//            animParser.parse();
+//
+//            mSequenceIdle = (SkeletalAnimationSequence) animParser.getParsedAnimationSequence();
+//
+//            mObject.setAnimationSequence(0);
+//            mObject.setFps(24);
+//            mObject.setScale(.04f);
+//            mObject.play();
+//
+//            getCurrentScene().addChild(mObject);
 
             /** 視角相機設定 */
             arcball = new ArcballCamera(mContext, ((Activity)mContext).findViewById(R.id.rejawali_layout));
             arcball.setPosition(INIT_CAMERA_X, INIT_CAMERA_Y, INIT_CAMERA_Z);
             getCurrentScene().replaceAndSwitchCamera(getCurrentCamera(), arcball);
+        } catch (ParsingException e){
+            Log.d("test", e.getMessage());
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -141,17 +181,26 @@ public class mRajawaliRenderer extends org.rajawali3d.renderer.RajawaliRenderer 
      * Model
      * */
     public void setModelPosition(double x, double y, double z){
-        watch.setPosition(x, y, z);
+        if (watch!=null)
+            watch.setPosition(x, y, z);
+
+        if (mObject!=null)
+            mObject.setPosition(x, y, z);
     }
     public void setModelScale(double scale){
-        watch.setScale(scale);
+        if (watch!=null)
+            watch.setScale(scale);
+
+        if (mObject!=null)
+            mObject.setScale(scale);
     }
     public void setModelRoateX(double rotx, double roty, double rotz){
-        try {
+
+        if (watch!=null)
             watch.setRotation(rotx, roty, rotz);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+
+        if (mObject!=null)
+            mObject.setRotation(rotx, roty, rotz);
     }
 
     /**
@@ -167,13 +216,15 @@ public class mRajawaliRenderer extends org.rajawali3d.renderer.RajawaliRenderer 
     @Override
     protected void onRender(long ellapsedRealtime, double deltaTime) {
         super.onRender(ellapsedRealtime, deltaTime);
-        mVideoTexture.update();
+//        mVideoTexture.update();
     }
 
     public void OnOff(){
-        if (mMediaPlayer.isPlaying())
-            mMediaPlayer.pause();
-        else
-            mMediaPlayer.start();
+        if (mMediaPlayer != null){
+            if (mMediaPlayer.isPlaying())
+                mMediaPlayer.pause();
+            else
+                mMediaPlayer.start();
+        }
     }
 }
